960a9c7f73c2410b163ce087a42df0e8b24ee676
DESKTOP-HALTBK4\Playdata <yjw127588@gmail.com>
2024-12-19 09:12:29 +0900

    백준 2557번 문제 풀이

---
+++ src/Baekjoon/B2557.java
@@ -0,0 +1,16 @@
+package Baekjoon;
+/* 시간 제한	메모리 제한	제출	정답	맞힌 사람	정답 비율
+1 초	128 MB	1268799	512990	353990	39.398%
+문제
+Hello World!를 출력하시오.
+입력
+없음
+
+출력
+Hello World!를 출력하시오.
+*/
+public class B2557 {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
