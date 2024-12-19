7791746dafd8a12a9741b4e171fca1063a85cd1f
DESKTOP-HALTBK4\Playdata <yjw127588@gmail.com>
2024-12-19 11:32:36 +0900

    백준 1000번 문제 풀이

---
+++ src/Baekjoon/B1000.java
@@ -0,0 +1,24 @@
+package Baekjoon;
+
+/* 두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.
+
+입력
+첫째 줄에 A와 B가 주어진다. (0 < A, B < 10)
+
+출력
+첫째 줄에 A+B를 출력한다.
+*/
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class B1000 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new java.io.InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        System.out.println(a + b);
+    }
+}
