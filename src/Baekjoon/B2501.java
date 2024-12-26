74af357e7f4ca964a917f1af1a456f4abf4a4481
DESKTOP-HALTBK4\Playdata <yjw127588@gmail.com>
2024-12-26 16:48:56 +0900

    백준 2501번 문제 풀이

---
+++ src/Baekjoon/B2501.java
@@ -0,0 +1,56 @@
+package Baekjoon;
+
+/*
+시간 제한	메모리 제한	제출	정답	맞힌 사람	정답 비율
+1 초	128 MB	80665	39933	35176	50.131%
+문제
+어떤 자연수 p와 q가 있을 때, 만일 p를 q로 나누었을 때 나머지가 0이면 q는 p의 약수이다.
+
+6을 예로 들면
+
+6 ÷ 1 = 6 … 0
+6 ÷ 2 = 3 … 0
+6 ÷ 3 = 2 … 0
+6 ÷ 4 = 1 … 2
+6 ÷ 5 = 1 … 1
+6 ÷ 6 = 1 … 0
+그래서 6의 약수는 1, 2, 3, 6, 총 네 개이다.
+
+두 개의 자연수 N과 K가 주어졌을 때, N의 약수들 중 K번째로 작은 수를 출력하는 프로그램을 작성하시오.
+
+입력
+첫째 줄에 N과 K가 빈칸을 사이에 두고 주어진다. N은 1 이상 10,000 이하이다. K는 1 이상 N 이하이다.
+
+출력
+첫째 줄에 N의 약수들 중 K번째로 작은 수를 출력한다. 만일 N의 약수의 개수가 K개보다 적어서 K번째 약수가 존재하지 않을 경우에는 0을 출력하시오.
+ */
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+public class B2501 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        br.close();
+
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+        ArrayList<Integer> numArray = new ArrayList<>();
+
+        for (int i = 1; i <= n; i++) {
+            if (n % i == 0) {
+                numArray.add(i);
+            }
+        }
+
+        if (numArray.size() < k) {
+            System.out.println("0");
+        } else {
+            System.out.println(numArray.get(k - 1));
+        }
+    }
+}
