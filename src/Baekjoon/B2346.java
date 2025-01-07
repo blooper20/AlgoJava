71407afc65b6bfa35c2e54bf12666ac80b179212
DESKTOP-HALTBK4\Playdata <yjw127588@gmail.com>
2025-01-07 11:24:49 +0900

    안쓰는 스트롱빌더 삭제

--- src/Baekjoon/B2346.java
+++ src/Baekjoon/B2346.java
@@ -27,7 +27,6 @@ import java.util.StringTokenizer;
 public class B2346 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
         Queue<int[]> baloons = new LinkedList<>();
 
         int N = Integer.parseInt(br.readLine());
