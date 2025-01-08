f973170023bd5b9b52f9a89b85f4a3f082b240d1
DESKTOP-HALTBK4\Playdata <yjw127588@gmail.com>
2025-01-08 11:16:06 +0900

    백준 1764번 문제 풀이

---
+++ src/Baekjoon/B1764.java
@@ -0,0 +1,53 @@
+package Baekjoon;
+
+/*
+시간 제한	메모리 제한	제출	정답	맞힌 사람	정답 비율
+2 초	256 MB	127542	55370	43275	41.702%
+문제
+김진영이 듣도 못한 사람의 명단과, 보도 못한 사람의 명단이 주어질 때, 듣도 보도 못한 사람의 명단을 구하는 프로그램을 작성하시오.
+
+입력
+첫째 줄에 듣도 못한 사람의 수 N, 보도 못한 사람의 수 M이 주어진다. 이어서 둘째 줄부터 N개의 줄에 걸쳐 듣도 못한 사람의 이름과, N+2째 줄부터 보도 못한 사람의 이름이 순서대로 주어진다. 이름은 띄어쓰기 없이 알파벳 소문자로만 이루어지며, 그 길이는 20 이하이다. N, M은 500,000 이하의 자연수이다.
+
+듣도 못한 사람의 명단에는 중복되는 이름이 없으며, 보도 못한 사람의 명단도 마찬가지이다.
+
+출력
+듣보잡의 수와 그 명단을 사전순으로 출력한다.
+ */
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class B1764 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        HashSet<String> noListen = new HashSet<>();
+        HashSet<String> noSee = new HashSet<>();
+
+        for (int i = 0; i < N; i++) {
+            noListen.add(br.readLine());
+        }
+
+        for (int i = 0; i < M; i++) {
+            noSee.add(br.readLine());
+        }
+        noListen.retainAll(noSee);
+
+        List<String> list = new ArrayList<>(noListen);
+        Collections.sort(list);
+
+        System.out.println(list.size());
+        list.forEach(System.out::println);
+    }
+}
