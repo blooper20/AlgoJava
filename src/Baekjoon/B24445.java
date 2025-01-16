e786621d3cdc145c894668fad72a036497334ed9
DESKTOP-HALTBK4\Playdata <yjw127588@gmail.com>
2025-01-16 17:46:09 +0900

    백준 24445번 문제 풀이

---
+++ src/Baekjoon/B24445.java
@@ -0,0 +1,105 @@
+package Baekjoon;
+
+/*
+시간 제한	메모리 제한	제출	정답	맞힌 사람	정답 비율
+1 초	512 MB	10275	6859	6239	69.023%
+문제
+오늘도 서준이는 너비 우선 탐색(BFS) 수업 조교를 하고 있다. 아빠가 수업한 내용을 학생들이 잘 이해했는지 문제를 통해서 확인해보자.
+
+N개의 정점과 M개의 간선으로 구성된 무방향 그래프(undirected graph)가 주어진다. 정점 번호는 1번부터 N번이고 모든 간선의 가중치는 1이다. 정점 R에서 시작하여 너비 우선 탐색으로 노드를 방문할 경우 노드의 방문 순서를 출력하자.
+
+너비 우선 탐색 의사 코드는 다음과 같다. 인접 정점은 내림차순으로 방문한다.
+
+bfs(V, E, R) {  # V : 정점 집합, E : 간선 집합, R : 시작 정점
+    for each v ∈ V - {R}
+        visited[v] <- NO;
+    visited[R] <- YES;  # 시작 정점 R을 방문 했다고 표시한다.
+    enqueue(Q, R);  # 큐 맨 뒤에 시작 정점 R을 추가한다.
+    while (Q ≠ ∅) {
+        u <- dequeue(Q);  # 큐 맨 앞쪽의 요소를 삭제한다.
+        for each v ∈ E(u)  # E(u) : 정점 u의 인접 정점 집합.(정점 번호를 내림차순으로 방문한다)
+            if (visited[v] = NO) then {
+                visited[v] <- YES;  # 정점 v를 방문 했다고 표시한다.
+                enqueue(Q, v);  # 큐 맨 뒤에 정점 v를 추가한다.
+            }
+    }
+}
+입력
+첫째 줄에 정점의 수 N (5 ≤ N ≤ 100,000), 간선의 수 M (1 ≤ M ≤ 200,000), 시작 정점 R (1 ≤ R ≤ N)이 주어진다.
+
+다음 M개 줄에 간선 정보 u v가 주어지며 정점 u와 정점 v의 가중치 1인 양방향 간선을 나타낸다. (1 ≤ u < v ≤ N, u ≠ v) 모든 간선의 (u, v) 쌍의 값은 서로 다르다.
+
+출력
+첫째 줄부터 N개의 줄에 정수를 한 개씩 출력한다. i번째 줄에는 정점 i의 방문 순서를 출력한다. 시작 정점의 방문 순서는 1이다. 시작 정점에서 방문할 수 없는 경우 0을 출력한다.
+ */
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class B24445 {
+    static List<List<Integer>> list = new ArrayList<List<Integer>>();
+    static int[] visited;
+    static int count = 1;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int R = Integer.parseInt(st.nextToken());
+
+        visited = new int[N + 1];
+
+        for (int i = 0; i <= N; i++) {
+            list.add(new ArrayList<>());
+        }
+
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            list.get(a).add(b);
+            list.get(b).add(a);
+        }
+
+        for (int i = 0; i <= N; i++) {
+            Collections.sort(list.get(i), Collections.reverseOrder());
+        }
+
+        bfs(R);
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 1; i <= N; i++) {
+            sb.append(visited[i]).append("\n");
+        }
+
+        System.out.println(sb);
+    }
+
+    static void bfs(int V) {
+        Queue<Integer> q = new LinkedList<>();
+        visited[V] = count++;
+        q.add(V);
+
+        while (!q.isEmpty()) {
+            int newV = q.poll();
+            for (int near : list.get(newV)) {
+                if (visited[near] == 0) {
+                    visited[near] = count++;
+                    q.add(near);
+                }
+            }
+        }
+    }
+}
