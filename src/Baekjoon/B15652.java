d290d46256f26c0b43ad00eb8e4be2de5e0638c0
blooper20 <yjw127588@gmail.com>
2025-01-27 15:18:40 +0900

    백준 15652번 문제 풀이

---
+++ src/Baekjoon/B15652.java
@@ -0,0 +1,62 @@
+package Baekjoon;
+
+/*
+시간 제한	메모리 제한	제출	정답	맞힌 사람	정답 비율
+1 초	512 MB	65202	51333	40731	78.683%
+문제
+자연수 N과 M이 주어졌을 때, 아래 조건을 만족하는 길이가 M인 수열을 모두 구하는 프로그램을 작성하시오.
+
+1부터 N까지 자연수 중에서 M개를 고른 수열
+같은 수를 여러 번 골라도 된다.
+고른 수열은 비내림차순이어야 한다.
+길이가 K인 수열 A가 A1 ≤ A2 ≤ ... ≤ AK-1 ≤ AK를 만족하면, 비내림차순이라고 한다.
+입력
+첫째 줄에 자연수 N과 M이 주어진다. (1 ≤ M ≤ N ≤ 8)
+
+출력
+한 줄에 하나씩 문제의 조건을 만족하는 수열을 출력한다. 중복되는 수열을 여러 번 출력하면 안되며, 각 수열은 공백으로 구분해서 출력해야 한다.
+
+수열은 사전 순으로 증가하는 순서로 출력해야 한다.
+ */
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class B15652 {
+    static int[] arr;
+    static StringBuilder sb;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        sb = new StringBuilder();
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        arr = new int[M];
+
+        dfs(N, M, 0,1);
+
+        System.out.println(sb.toString());
+    }
+
+    static void dfs(int N, int M, int depth, int start) {
+        if (depth == M) {
+            for (int num : arr) {
+                sb.append(num).append(" ");
+            }
+
+            sb.append("\n");
+
+            return;
+        }
+
+        for (int i = start; i <= N; i++) {
+            arr[depth] = i;
+            dfs(N, M, depth + 1, i);
+        }
+    }
+}
